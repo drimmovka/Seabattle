ecb411865191d2e97238977164a64efad7539f38
drimmovka <144445070+drimmovka@users.noreply.github.com>
2024-10-02 19:51:25 +0300

    Delete Battleship directory

--- Battleship/CMakeFiles/battleship.dir/src/collision_handler.cpp.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/battleship.dir/src/collision_handler.cpp.o: \
- /mnt/c/Users/drimm/WorkSpace/Battleship/src/collision_handler.cpp \
- /usr/include/stdc-predef.h \
- /mnt/c/Users/drimm/WorkSpace/Battleship/src/../include/collision_handler.hpp \
- /mnt/c/Users/drimm/WorkSpace/Battleship/src/../include/coords.hpp \
- /mnt/c/Users/drimm/WorkSpace/Battleship/src/../include/entity.hpp
--- Battleship/include/game_renderer.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef GAME_RENDERER_HPP_
-#define GAME_RENDERER_HPP_
-
-
-
-#endif
